77837ba0dd931c618cb88ad13fbac5915f03ff49
Kenny Kerr <kenny.kerr@microsoft.com>
2020-07-15 08:30:53 -0700

    dx

---
+++ Samples/build.bat
@@ -0,0 +1,16 @@
+cl /nologo /W4 MakeResource.cpp
+MakeResource.exe Image.gif > Image.cpp
+
+cl /nologo /W4 Animation.cpp
+cl /nologo /W4 AnimationEfficient.cpp
+cl /nologo /W4 BitmapBrush.cpp Image.cpp
+cl /nologo /W4 CreateBitmapFromWicBitmap.cpp Image.cpp
+cl /nologo /W4 CreateImageEncoder.cpp
+cl /nologo /W4 DesktopDeviceContext.cpp
+cl /nologo /W4 HelloWorld.cpp
+cl /nologo /W4 HwndRenderTarget.cpp
+cl /nologo /W4 LinearGradient.cpp
+cl /nologo /W4 RadialGradient.cpp
+cl /nologo /W4 WicBitmapRenderTarget.cpp
+
+del *.obj
